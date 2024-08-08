79aeae124b50d386435144b27ad94f9bc5a04f7f
Jax Williams <jax@apus.network>
2024-08-08 23:14:18 +0800

    fix auto script exit bug

--- scripts/auto-eva.ts
+++ scripts/auto-eva.ts
@@ -1,41 +1,51 @@
-import { ModelID, PoolProcess, SiqaProcess, WrappedARProcess } from "./config"
-import { msgResultWrapper, commonSigner } from "./wallet"
+import { ModelID, PoolProcess, SiqaProcess, WrappedARProcess } from "./config";
+import { msgResultWrapper, commonSigner } from "./wallet";
 
-const msgResultCommon = msgResultWrapper(commonSigner)
+const msgResultCommon = msgResultWrapper(commonSigner);
 
 async function Evaluate() {
-    console.log(`Running Evaluate at ${new Date().toISOString()}`);
-    const result = await msgResultCommon(PoolProcess, {
-        Action: "Evaluate"
-    }, 2)
-    console.log(result)
+  console.log(`Running Evaluate at ${new Date().toISOString()}`);
+  const result = await msgResultCommon(
+    PoolProcess,
+    {
+      Action: "Evaluate",
+    },
+    2
+  );
+  console.log(result);
 }
 
 async function AllocateRewards() {
-    console.log(`Running AllocateRewards at ${new Date().toISOString()}`);
-    const result = await msgResultCommon(PoolProcess, {
-        Action: "Allocate-Rewards"
-    }, 2)
-    console.log(result)
+  console.log(`Running AllocateRewards at ${new Date().toISOString()}`);
+  const result = await msgResultCommon(
+    PoolProcess,
+    {
+      Action: "Allocate-Rewards",
+    },
+    2
+  );
+  console.log(result);
 }
 
 function runTaskAtInterval(task: () => Promise<void>, interval: number) {
-    task().catch(console.error); // 立即运行一次
-    setInterval(() => {
-        task().catch(console.error);
-    }, interval);
+  task().catch(console.error); // 立即运行一次
+  setInterval(() => {
+    task().catch(console.error);
+  }, interval);
 }
 
-const main = async () => {
+const evaluateInterval = 60 * 60 * 1000; // 60分钟
+const allocateInterval = 12 * 60 * 60 * 1000; // 12小时
 
-    const evaluateInterval = 60 * 60 * 1000; // 60分钟
-    const allocateInterval = 12 * 60 * 60 * 1000; // 12小时
+runTaskAtInterval(Evaluate, evaluateInterval);
+runTaskAtInterval(AllocateRewards, allocateInterval);
 
-    runTaskAtInterval(Evaluate, evaluateInterval);
-    runTaskAtInterval(AllocateRewards, allocateInterval);
-};
+process.on("SIGINT", () => {
+  console.log("Process interrupted. Exiting gracefully...");
+  process.exit();
+});
 
-main().catch(error => {
-    console.error("An error occurred:", error);
-    process.exit(1);
+process.on("SIGTERM", () => {
+  console.log("Process terminated. Exiting gracefully...");
+  process.exit();
 });
