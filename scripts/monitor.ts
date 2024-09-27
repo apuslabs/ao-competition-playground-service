60339346fe723ce124c6789b3e0f4009c7fcd23c
wiz <wizdaydream@gmail.com>
2024-09-27 17:06:26 +0800

    Revert "Add monitors"
    This reverts commit 2832d59ecb70739bd37c09e4a19394278ba9766e.

--- scripts/monitor.ts
+++ scripts/monitor.ts
@@ -37,117 +37,16 @@ Total participants: \`${total}\``,
   });
 }
 
-async function getAllPoolStatistic() {
-  const result = await dryrun(POOL_PROCESS, {
-    Action: 'Participants-All-Statistic',
-  });
-  const { last_hour, last_day, total } = JSON.parse(
-    result?.Messages?.[0]?.Data ?? '{}'
-  );
-  console.log(last_hour, last_day, total);
-  //   axios.post(webhookUrl, {
-  //     text: `*Pool Statistic*:\n
-  // Participants in last hour: \`${last_hour}\`\n
-  // Participants in last 24h: \`${last_day}\`\n
-  // Total participants: \`${total}\``,
-  //   });
-}
-
-async function getDatasetStatistic() {
-  const result = await dryrun(POOL_PROCESS, {
-    Action: 'Ongoing-CompetitionIds',
-  });
-
-  const ongoingCompetitions = JSON.parse(result?.Messages?.[0]?.Data) ?? [];
-
-  let text = '************ Dataset-Statistics ************\n';
-
-  for (const comp of ongoingCompetitions) {
-    text = text + comp.Title + '(' + comp.Id + '):\n';
-    const compProcess = comp['Process'] ?? '';
-    if (!compProcess || compProcess == '') {
-      continue;
-    }
-    const fetchDatasetRes = await dryrun(compProcess, {
-      Action: 'Dataset-Statistic',
-    });
-
-    const datasetRes = JSON.parse(fetchDatasetRes?.Messages?.[0]?.Data) ?? {};
-    const datasets = datasetRes.data ?? [];
-
-    text = text + '\nEvaluated:\n';
-    datasets
-      .filter((d: any) => {
-        return d.finished_evaluations == d.total_evaluations;
-      })
-      .forEach((d: any) => {
-        text =
-          text +
-          d.participant_dataset_hash +
-          ' (' +
-          d.pending_evaluations +
-          '|' +
-          d.processing_evaluations +
-          '|' +
-          d.finished_evaluations +
-          '|=' +
-          d.total_evaluations +
-          '=)\n';
-      });
-
-    text = text + '\nEvaluating:\n';
-    datasets
-      .filter((d: any) => {
-        return d.pending_evaluations < d.total_evaluations;
-      })
-      .forEach((d: any) => {
-        text =
-          text +
-          d.participant_dataset_hash +
-          ' (' +
-          d.pending_evaluations +
-          '|' +
-          d.processing_evaluations +
-          '|' +
-          d.finished_evaluations +
-          '|=' +
-          d.total_evaluations +
-          '=)\n';
-      });
-
-    text = text + '\nPendinng:\n';
-    datasets
-      .filter((d: any) => {
-        return d.pending_evaluations == d.total_evaluations;
-      })
-      .forEach((d: any) => {
-        text =
-          text +
-          d.participant_dataset_hash +
-          ' (' +
-          d.pending_evaluations +
-          '|' +
-          d.processing_evaluations +
-          '|' +
-          d.finished_evaluations +
-          '|=' +
-          d.total_evaluations +
-          '=)\n';
-      });
-  }
-}
-
 async function main() {
   getHerderStatistic();
   getPoolStatistic();
-  getAllPoolStatistic();
-  getDatasetStatistic();
-  setInterval(() => {
-    getHerderStatistic();
-    getPoolStatistic();
-    getAllPoolStatistic();
-    getDatasetStatistic();
-  }, 1000 * 60 * 60 * 1);
+  setInterval(
+    () => {
+      getHerderStatistic();
+      getPoolStatistic();
+    },
+    1000 * 60 * 60 * 1,
+  );
 }
 
 main();
