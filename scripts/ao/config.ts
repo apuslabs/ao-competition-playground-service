5193b0b6c3235977ffdf047bc66cd12606553fae
Jax Williams <17276029+jax-cn@users.noreply.github.com>
2024-11-29 15:36:36 +0800

    update config

--- scripts/ao/config.ts
+++ scripts/ao/config.ts
@@ -1,4 +1,4 @@
-export const EMBEDDING_PROCESS = 'vp4pxoOsilVxdsRqTmLjP86CwwUwtj1RoKeGrFVxIVk';
-export const HERDER_PROCESS = 'IdHBRpDM4rgkLbO0oNU7cErvDQ7DdcCrQo8b-3Rd6io';
-export const POOL_PROCESS = 'jzZzZJ6SpxLKaZv8rx2rmkq-QwKGQliFyWqr-OK9CIo';
+export const EMBEDDING_PROCESS = 'q7QJTZbOI_avjaltnF-kP21kpL3qCs8sJSP6Wdl9wzM';
+export const HERDER_PROCESS = 'J0hmRtTPfV3NFiLvn1Pw9-KqrfeAzRAE2JkKWQjz3t0';
+export const POOL_PROCESS = 'HJV6BxQGakxlQe6rfGxOMiifKSFYRz5WEwZOKPLPWEg';
 export const EMBEDDING_SERVICE = 'http://127.0.0.1:8082';
