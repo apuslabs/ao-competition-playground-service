f7be88f4dcd69aa407e85c435ffe53a8076c65c8
Jax <jax@apus.network>
2025-02-12 17:59:15 +0800

    update the rag process id for oracle

--- scripts/ao/config.ts
+++ scripts/ao/config.ts
@@ -1,4 +1,4 @@
-export const EMBEDDING_PROCESS = '9Ko9atpgC_2Al4-ut9jhSHo5vG8Yqg3Y49g-C17lB90';
+export const EMBEDDING_PROCESS = 'fEP7m9HKlDzrKehnPM0jzJzJZ0nIfgrH5KlY0PlHJKc';
 export const HERDER_PROCESS = 'J0hmRtTPfV3NFiLvn1Pw9-KqrfeAzRAE2JkKWQjz3t0';
 export const POOL_PROCESS = 'HJV6BxQGakxlQe6rfGxOMiifKSFYRz5WEwZOKPLPWEg';
 export const EMBEDDING_SERVICE = 'http://127.0.0.1:8082';
